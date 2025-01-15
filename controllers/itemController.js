dfc4ac82f3b196b08cf59a81b90c86b35d5599ab
andrew <spacedaoist@gmail.com>
2025-01-15 21:04:51 +0000

    added get all items route and function

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -12,6 +12,17 @@ exports.readItemsByUser = async (req, res) => {
     }
 }
 
+exports.readItems = async (req, res ) => {
+    try {
+        const items = await prisma.item.findMany();
+        if(items == null) return res.status(404).json({ error: 'No items found' });
+        return res.status(200).json(items);
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
 // This controller function will create a new item after validating the accessToken
 exports.createItem = async (req, res) => {
     try {
--- routes/itemRoute.js
+++ routes/itemRoute.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const itemController = require('../controllers/itemController');
 
+router.get('/', itemController.readItems);
 router.post('/', itemController.createItem);
 
 module.exports = router;
