683f1f6e3324bcf9c894956d0a166ebe628f6c97
andrew <spacedaoist@gmail.com>
2025-01-15 22:32:53 +0000

    added delete and update functions with routes

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -35,6 +35,8 @@ exports.readItemsByUser = async (req, res) => {
     }
 }
 
+// This controller function will find all items in the db
+// Each item will also include the id and username of the respective inventory manager
 exports.readItems = async (req, res ) => {
     try {
         const items = await prisma.item.findMany({
@@ -49,6 +51,26 @@ exports.readItems = async (req, res ) => {
         });
         if(items.length == 0) return res.status(404).json({ error: 'No items found' });
         return res.status(200).json(items);
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+// This controller function will retrieve the item details from an id param
+exports.readItemById = async (req, res) => {
+    try {
+        const id = parseInt(req.params.id);
+
+        const item = await prisma.item.findUnique({
+            where: {
+                id: id
+            }
+        });
+
+        if(item == null) return res.status(404).json({ error: 'Item not found' });
+
+        return res.status(200).json(item);
 
     } catch (error) {
         return res.status(500).json({ error: error.message });
@@ -98,6 +120,104 @@ exports.createItem = async (req, res) => {
 
         // Set the response with new item data
         return res.status(201).json(newItem);
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+// This controller function will verify the user and the item userId
+// then it will update the item with the respective fields
+exports.updateItem = async (req, res) => {
+    try {
+        
+        if(req.body.itemName !== undefined && req.body.itemName.trim() === '') {
+            return res.status(422).json({ error: 'Item name cannot be empty' });
+        }
+
+        if(req.body.quantity !== undefined && (typeof req.body.quantity !== 'number') || req.body.quantity < 1){
+            return res.status(422).json({ error: 'Quantity must be a non-zero and non-negative number' });
+        }
+
+        // Verify the token and extract the user information
+        const authHeader = req.headers['authorization'];
+        if(authHeader == undefined) return res.status(401).json({ error: 'Unauthorized' });
+        const token = authHeader && authHeader.split(' ')[1];
+        if (token == null) return res.status(401).json({ error: 'Unauthorized' });
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if(err) return res.status(403);
+            req.user = user;
+            console.log(`${user.username} was verified`);
+        });
+
+        if(req.user == undefined) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+
+        const oldItem = await prisma.item.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+
+        if(oldItem.userId != req.user.id){
+            return res.status(401).json({ error: 'Unauthorized update' });
+        }
+        
+        const updatedItem = await prisma.item.update({
+            data: {
+                userId: req.user.id,
+                itemName: req.body.itemName,
+                description: req.body.description,
+                quantity: req.body.quantity
+            },
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+
+        return res.status(200).json(updatedItem);
+
+    } catch (error) {
+        return res.status(500).json({ error: error.message });
+    }
+}
+
+// This function will verify the user and delete the requested item from the db
+exports.deleteItem = async (req, res) => {
+    try {
+        // Verify the token and extract the user information
+        const authHeader = req.headers['authorization'];
+        if(authHeader == undefined) return res.status(401).json({ error: 'Unauthorized' });
+        const token = authHeader && authHeader.split(' ')[1];
+        if (token == null) return res.status(401).json({ error: 'Unauthorized' });
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+            if(err) return res.status(403);
+            req.user = user;
+            console.log(`${user.username} was verified`);
+        });
+
+        if(req.user == undefined) {
+            return res.status(401).json({ error: 'Unauthorized' });
+        }
+        
+        const oldItem = await prisma.item.findUnique({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+
+        if(oldItem.userId != req.user.id){
+            return res.status(401).json({ error: 'Unauthorized delete' });
+        }
+
+        await prisma.item.delete({
+            where: {
+                id: parseInt(req.params.id)
+            }
+        });
+
+        return res.status(202).json({ message: 'Item deleted successfully' });
 
     } catch (error) {
         return res.status(500).json({ error: error.message });
--- routes/itemRoute.js
+++ routes/itemRoute.js
@@ -4,5 +4,8 @@ const itemController = require('../controllers/itemController');
 router.get('/', itemController.readItems);
 router.post('/', itemController.createItem);
 router.get('/user', itemController.readItemsByUser);
+router.get('/:id', itemController.readItemById);
+router.put('/:id', itemController.updateItem);
+router.delete('/:id', itemController.deleteItem);
 
 module.exports = router;
